b2167f42cec2a4129167c82c06ec5334a75cb360
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-02-23 22:00:32 +0100

    Create compInt.js

---
+++ compInt.js
@@ -0,0 +1,33 @@
+
+let pv = document.querySelector(".pv")
+let yr = document.querySelector(".yr")
+let fv = document.querySelector(".fv")
+let intr = document.querySelector(".int")
+let btn = document.querySelector(".btn")
+
+
+btn.addEventListener("click",()=>{
+    
+       
+})
+
+
+function compInt(){
+let pValue = parseInt(pv.value)
+     let fValue = parseInt(fv.value)
+      let intR = parseInt(intr.value)
+       let yR = parseInt(yr.value)
+       
+let result = (1+((intR)/n))**(n*r)
+
+return p * result 
+
+    
+}
+
+console.log(compInt(100,10,5))
+
+/*
+A = P (1 + r/n)**n
+
+*/
