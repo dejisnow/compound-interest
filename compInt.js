cbfc17cce25dfaf2ef2e3f05eead1d82bb18eb37
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-02-24 22:25:00 +0100

    Update compInt.js

--- compInt.js
+++ compInt.js
@@ -1,13 +1,17 @@
+window.onload= ()=>{
+    
+
 
 let pv = document.querySelector(".pv")
 let yr = document.querySelector(".yr")
 let fv = document.querySelector(".fv")
 let intr = document.querySelector(".int")
 let btn = document.querySelector(".btn")
+let ans = document.querySelector(".ans")
 
 
 btn.addEventListener("click",()=>{
-    
+   fv.value = compInt() 
        
 })
 
@@ -17,17 +21,21 @@ let pValue = parseInt(pv.value)
      let fValue = parseInt(fv.value)
       let intR = parseInt(intr.value)
        let yR = parseInt(yr.value)
+       if(pValue)
        
-let result = (1+((intR)/n))**(n*r)
+let result = (1+(intR/100))**(yR)
 
-return p * result 
+return pValue * result 
+
 
     
 }
 
-console.log(compInt(100,10,5))
+
 
 /*
 A = P (1 + r/n)**n
 
 */
+
+}
